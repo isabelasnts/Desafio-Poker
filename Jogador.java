fb47435eedfce9e9c40fed0bb2acdb05f1e80429
isabelasnts <101901724+isabelasnts@users.noreply.github.com>
2022-11-27 12:28:45 -0300

    Update Jogador.java

--- Jogador.java
+++ Jogador.java
@@ -5,52 +5,61 @@ public class Jogador {
 
     private Carta[] mao = new Carta[2];
     private double dinheiro = 100.00;
+
     public Jogador() {
     }
-    public void Inapto(){
+
+    public void Inapto() {
         this.Apto = false;
     }
-    public boolean getApto(){
+
+    public boolean getApto() {
         return this.Apto;
     }
-    public double getAposta(){
+
+    public double getAposta() {
         return this.Aposta;
     }
-    public void addAposta(double Aposta){
+
+    public void addAposta(double Aposta) {
         this.Aposta += Aposta;
     }
-    public void zeraAposta(){
+
+    public void zeraAposta() {
         this.Aposta = 0;
     }
+
     public Jogador insereNome(String Nome) {
         this.Nome = Nome;
         return this;
     }
+
     public void imprimeJogador() {
-        System.out.println("Jogador " + this.Nome+" possui as cartas: " + this.mao[0].get()+ " " +this.mao[1].get()+" e $"+ this.dinheiro + " no pote");
+        System.out.println("\nJogador " + this.Nome + " possui as cartas: " + this.mao[0].get() + " " + this.mao[1].get() + " e $" + this.dinheiro + " no pote");
     }
+
     public void insereCarta(int pos, Carta carta) {
         this.mao[pos] = carta;
     }
+
     public boolean retiraDinheiro(double retirada) {
-       if (retirada>this.dinheiro){
-           return false;
-       }
+        if (retirada > this.dinheiro) {
+            return false;
+        }
         this.dinheiro -= retirada;
         return true;
     }
+
     public void addDinheiro(double deposito) {
         this.dinheiro += deposito;
     }
+
     public String getNome() {
-       return this.Nome;
+        return this.Nome;
     }
+
     public double getSaldo() {
         return this.dinheiro;
     }
 
 }
-
-
-
-
